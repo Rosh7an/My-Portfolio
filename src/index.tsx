58b665bea6dee0eb181868c5e59ec2e0c7955e18
Roshan J <roshan.jabbar@gadgeon.com>
2023-08-03 11:18:04 +0530

    removed unwanted codes

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
